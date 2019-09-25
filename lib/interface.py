f0975b2a68f3c54d571600ec5c193ea7f8fba7cc
Yuki Matsumoto <bchcoffee@gmail.com>
2019-09-25 13:39:43 -0400

    Some typos fixed and remove requirement for index.html

--- lib/interface.py
+++ lib/interface.py
@@ -310,7 +310,7 @@ class Interface(util.PrintError):
         self.pipe.clean_up()
 
     def queue_request(self, *args):  # method, params, _id
-        '''Queue a request, later to be send with send_requests when the
+        '''Queue a request, later to be sent with send_requests when the
         socket is available for writing.
         '''
         self.request_time = time.time()
